3783b09a904a05ba104efa167d694c68a13324ae
ChrisBAshton <chrisashtonweb@gmail.com>
2016-02-24 08:03:12 +0000

    first attempt. Hopefully it's fully formed.

---
+++ delete-merged-branches.js
@@ -0,0 +1,23 @@
+module.exports = function (data, process) {
+
+    if (data.payload.action === 'closed' && data.payload.pull_request.merged) {
+
+        var options = {
+            url: data.payload.git_refs_url.replace('{/sha}', '/heads/' + data.payload.pull_request.head.ref),
+            headers: {
+                'Content-Type':  'application/json',
+                'User-Agent':    'delete-merged-branches',
+                'Authorization': 'token ' + data.access_token
+            }
+        };
+
+        request.delete(options, function branchDeleted(err, httpResponse, body) {
+            if (err) {
+                process.fail('Could not send POST request: ' + err);
+            }
+            else {
+                process.succeed('DELETE message successful. Response:' + body);
+            }
+        });
+    }
+};
